6562262371b4507570eec57530209a72d76bb99e
OZoneGuy <oalkersh@protonmail.com>
2022-04-18 00:02:53 -0400

    chore - Formatted file

--- src/core/db.rs
+++ src/core/db.rs
@@ -1,5 +1,5 @@
+use log::{debug, error, info};
 use neo4rs::*;
-use log::{error, info, debug};
 
 pub struct Database {
     graph_db: Graph,
@@ -8,14 +8,17 @@ pub struct Database {
 impl Database {
     pub async fn new(cfg: Config) -> Self {
         debug!("Attempting connection with config: {:?}", cfg);
-        let new_graph = Graph::new(format!("{}:{}", cfg.address, cfg.port).as_str(), cfg.username.as_str(), cfg.pass.as_str()).await;
+        let new_graph = Graph::new(
+            format!("{}:{}", cfg.address, cfg.port).as_str(),
+            cfg.username.as_str(),
+            cfg.pass.as_str(),
+        )
+        .await;
         match new_graph {
             Ok(g) => {
                 info!("Connected to database!");
-                Database {
-                    graph_db: g
-                }
-            },
+                Database { graph_db: g }
+            }
             Err(e) => {
                 error!("Failed to connect to the database: {:#?}", e);
                 panic!("Failed to connect to the database: {:#?}", e);
@@ -24,9 +27,10 @@ impl Database {
     }
 
     pub async fn get_topics(&self) -> Result<Vec<String>> {
-        let res = self.graph_db.execute(
-            query("MATCH (t: Topic) RETURN t")
-        ).await;
+        let res = self
+            .graph_db
+            .execute(query("MATCH (t: Topic) RETURN t"))
+            .await;
         match res {
             Ok(mut r) => {
                 let mut topics: Vec<String> = vec![];
@@ -38,10 +42,8 @@ impl Database {
                 }
 
                 return Ok(topics);
-            },
-            Err(e) => {
-                Err(e)
             }
+            Err(e) => Err(e),
         }
     }
 }
@@ -51,7 +53,7 @@ pub struct Config {
     pub address: String,
     pub port: String,
     pub username: String,
-    pub pass: String
+    pub pass: String,
 }
 
 impl Default for Config {
@@ -60,7 +62,7 @@ impl Default for Config {
             address: "localhost".to_string(),
             port: "7687".to_string(),
             username: "admin".to_string(),
-            pass: "".to_string()
+            pass: "".to_string(),
         }
     }
 }
