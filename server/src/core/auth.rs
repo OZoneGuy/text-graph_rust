12f5ddfe0c745a3255450d5cac841100198c871b
OZoneGuy <oalkersh@protonmail.com>
2022-04-20 22:33:15 -0400

    feat - Added debugging logs to auth guard

--- server/src/core/auth.rs
+++ server/src/core/auth.rs
@@ -1,5 +1,6 @@
 use rocket::request::{ FromRequest, Outcome, Request };
 use rocket::http::{ Cookie, Status };
+use log::{ debug };
 
 pub struct AuthHandler {
 }
@@ -15,14 +16,18 @@ impl<'r> FromRequest<'r> for AuthHandler {
     type Error = AuthError;
     async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
         if request.uri().path() == "/login" {
+            debug!("Entering Auth guard in /login endpoint");
             request.cookies().add(Cookie::new("auth", "auth"));
 
             return Outcome::Success(AuthHandler{});
         }
 
+        debug!("Running the auth gaurd on a different endpoint");
         if let Some(_) = request.cookies().get("auth") {
+            debug!("Successful authentication");
             Outcome::Success(AuthHandler{})
         } else {
+            debug!("Failed to authenticate");
             Outcome::Failure((Status::new(403), AuthError::Unauthorised ))
         }
     }
