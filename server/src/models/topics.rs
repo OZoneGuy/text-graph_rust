a95b9efc2e1a13dd1b4937c2bee77d74a1df1226
OZoneGuy <oalkersh@protonmail.com>
2022-05-29 00:13:18 -0400

    feat - Added topic struct for arango

--- server/src/models/topics.rs
+++ server/src/models/topics.rs
@@ -1,7 +1,13 @@
+use arangoq::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct NewTopic {
     pub name: String,
     pub id: Option<String>,
+}
+
+#[derive(ArangoBuilder, Serialize)]
+pub struct Topic {
+    name: String,
 }
